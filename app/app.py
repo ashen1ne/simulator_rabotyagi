d47c69d3b46c19d10e557f58a9f56473510c468f
ashen1ne <godzilaspomoyki@gmail.com>
2026-03-22 21:16:49 +0300

    chore: create db.py for work with db

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.db import Base, create_db_and_tables
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
